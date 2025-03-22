8eff81320773840e23a955ddcb658951cb8b2a36
Anmol <anmolbansal1969@gmail.com>
2025-03-22 17:33:27 +0530

    fixing bill

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -8,31 +8,35 @@ class TripBillSerializer(serializers.ModelSerializer):
 
 class TripSerializer(serializers.ModelSerializer):
     bills = TripBillSerializer(many=True, read_only=True)
-    
+    bill_images = serializers.ListField(
+        child=serializers.FileField(),
+        write_only=True,  # This field is only for input, not output
+        required=False,
+    )
+
     class Meta:
         model = Trip
         fields = [
             'id', 'surveyor_name', 'driver_name', 'start_odometer_reading', 
             'vehicle_number', 'start_date', 'start_location',
             'end_odometer_reading', 'end_location', 'end_date',
-            'bills'
+            'bills', 'bill_images'  # Add bill_images to fields
         ]
-    
-    def create(self, validated_data):
-        return Trip.objects.create(**validated_data)
-    
+
     def update(self, instance, validated_data):
+        # Remove bill_images from validated_data to avoid errors
+        bill_images = validated_data.pop('bill_images', None)
+
         # Update trip fields
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
-        
+
         # Handle bill images
-        bill_images = self.context['request'].FILES.getlist('bill_images')
         if bill_images:
             for image in bill_images:
                 TripBill.objects.create(trip=instance, image=image)
-        
+
         return instance
 
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,26 +1,10 @@
 from rest_framework import viewsets
 from rest_framework.parsers import MultiPartParser, FormParser
-from .models import Trip, TripBill
+from .models import Trip
 from .serializers import TripSerializer
-from django.shortcuts import render
-
-def form(request):
-    return render(request, 'bill.html')
 
 class TripViewSet(viewsets.ModelViewSet):
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
-    parser_classes = [MultiPartParser, FormParser]
-
-    def get_object(self):
-        # Override get_object to handle the PATCH request correctly
-        obj = super().get_object()
-        return obj
-
-    def partial_update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data)
+    parser_classes = [MultiPartParser, FormParser]  # Allow file uploads
 
